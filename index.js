06ed1166ff1b2f254b61b270f476fc8da3f9cd03
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2019-06-10 22:14:47 +0300

    Init commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const request = require("request");
+const cheerio = require("cheerio");
+const PORT = 8888;
+
+const visit = (url, cb) => {
+  request.get(url, (err, res, data) => {
+    const $ = cheerio.load(data);
+
+    let results = []
+
+    $('a.childVifUrl').each((i, el) => {
+      const make = $(el).attr('data-make');
+      const model = $(el).attr('data-model');
+      const price = $(el).attr('data-price');
+      const year = $(el).attr('data-year');
+      const mileage = $(el).attr('data-mileage');
+      const url = $(el).attr('href');
+      const id = $(el).attr('data-id');
+      let images = [];
+
+      $(`a[href='${url}']`).each((i, el) => {
+        const image = $(el).attr('alt');
+
+        if (image) {
+          images.push(image);
+        }
+      });
+
+      results.push({
+        make, model, year, price, mileage, url, id, images
+      });
+    });
+
+    cb(results);
+  });
+}
+
+app.use('/', express.static(path.join(__dirname, '/client')));
+app.get('/cars', (req, res) => {
+  try {
+    visit(req.query.url, (data) => {
+      res.json(data);
+    });
+  } catch (e) {
+    res.send('nope...');
+  }
+});
+
+app.listen(PORT);
