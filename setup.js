30a813675353e67f39ddb6348765d547916e7ee5
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2019-06-15 12:06:07 +0300

    Add setup

---
+++ setup.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const SEARCH_URLS_FILENAME = 'searchUrls.txt';
+const DB_FILENAME = 'db.json';
+const DOT_ENV_FILENAME = '.env';
+
+const dbExists = fs.existsSync(`./${DB_FILENAME}`);
+const searcUrlsExists = fs.existsSync(`./${SEARCH_URLS_FILENAME}`);
+const dotEnvExists = fs.existsSync(`./${DOT_ENV_FILENAME}`);
+
+if (!dbExists) {
+  const db = { carIds: [] };
+  fs.writeFileSync(DB_FILENAME, JSON.stringify(db), 'utf8');
+};
+
+if (!searcUrlsExists) {
+  fs.writeFileSync(SEARCH_URLS_FILENAME, '', 'utf8');
+}
+
+if (!dotEnvExists) {
+  const dotEnvTemplate = `EMAIL=
+  EMAIL_PASSWORD=
+  RECIPIENT_EMAIL=`;
+  fs.writeFileSync(DOT_ENV_FILENAME, dotEnvTemplate, 'utf8');
+}
